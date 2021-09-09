95a98a6d2f2c8ba43fc751f7cfa2da968cae6688
kwiebe <katharina.wiebe@maibornwolff.de>
2021-09-09 15:30:57 +0200

    beispiel: cy studio fertig

--- cypress/integration/beispiel-testrunner.spec.js
+++ cypress/integration/beispiel-testrunner.spec.js
@@ -32,22 +32,5 @@ context("beispiele zu folien", () => {
             cy.get('[data-test="signin-submit"]').should("be.enabled").click();
             cy.url().should("include", "/singin");
         });
-
-        /* it("beispiel mit der google seite", () => {
-            cy.visit("https://www.google.de");
-            cy.get('#L2AGLb > .jyfHyd').click();
-            cy.get('#hpctaplay').should("exist").and("be.visible");
-            cy.get('.gLFyf').should("exist");
-            cy.get('.FPdoLc > center > .gNO89b').should("exist");
-            cy.get('#gbqfbb').should("exist");
-            cy.get('.gb_3').should("exist");
-        })
-    
-        it("my second test", () => {
-            cy.visit("https://www.google.de");
-            cy.get('#L2AGLb > .jyfHyd').click();
-            cy.get('.gLFyf').clear().type("cucumber test");
-            cy.get('.aajZCb > .lJ9FBc > center > .gNO89b').click().end();
-        }) */
     })
 })
